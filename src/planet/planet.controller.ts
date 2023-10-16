6af40bd40598d56cf3cb96e0038474f40e1941a1
hyeongyu-kim <rlagusrb619@gmail.com>
2023-10-16 18:13:02 +0900

    Feat: 행성 생성 로직 추가

--- src/planet/planet.controller.ts
+++ src/planet/planet.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Param, Post, Request } from '@nestjs/common';
+import { PlanetService } from './planet.service';
 
 @Controller('planet')
-export class PlanetController {}
+export class PlanetController {
+  constructor(private readonly planetService: PlanetService) {}
+
+  @Post('join/:planetId')
+  async joinPlanet(
+    @Request() req,
+    @Param('planetId') planetId: number,
+  ): Promise<any> {
+    const userId = req.user.id;
+    await this.planetService.joinPlanet(userId, planetId);
+    return { message: '행성에 성공적으로 가입되었습니다.' };
+  }
+}
--- src/planet/planet.service.ts
+++ src/planet/planet.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class PlanetService {}
+export class PlanetService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async joinPlanet(userId: number, planetId: number): Promise<boolean> {
+    const existingMembership = await this.prisma.planetMembership.findUnique({
+      where: {
+        planetId_userId: {
+          userId: userId,
+          planetId: planetId,
+        },
+      },
+    });
+
+    if (existingMembership) {
+      throw new Error('사용자는 이미 해당 행성에 가입되어 있습니다.');
+    }
+
+    await this.prisma.planetMembership.create({
+      data: {
+        userId: userId,
+        planetId: planetId,
+        administrator: false,
+      },
+    });
+
+    return true;
+  }
+}
