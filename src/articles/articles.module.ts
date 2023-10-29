efdb3758d05b6d6e299b5be6427d768a2720c82a
hyeongyu-kim <rlagusrb619@gmail.com>
2023-10-29 18:25:02 +0900

    Fix: view-count 모듈 임포트 오류 해결

--- src/articles/articles.module.ts
+++ src/articles/articles.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { ArticlesService } from './articles.service';
 import { ArticlesController } from './articles.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { ViewCountService } from 'src/view-count/view-count.service';
+import { ViewCountModule } from 'src/view-count/view-count.module';
 
 @Module({
-  imports: [ViewCountService],
+  imports: [ViewCountModule],
   providers: [ArticlesService, PrismaService],
   controllers: [ArticlesController],
 })
