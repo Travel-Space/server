dd80e7b29c37dd57608c46c69620cd81ac9d25e8
Hyeongyu-619 <110807563+Hyeongyu-619@users.noreply.github.com>
2023-10-30 20:50:06 +0900

    Feat: 내 댓글 삭제/관리자 댓글 삭제 추가

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -1,7 +1,10 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
+  HttpCode,
+  HttpStatus,
   Param,
   ParseIntPipe,
   Post,
@@ -12,7 +15,7 @@ import {
 } from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiBody } from '@nestjs/swagger';
 import { CreateCommentDto, UpdateCommentDto } from './dto';
-import { JwtAuthGuard, LoggedInGuard } from 'src/auth/guard';
+import { AdminGuard, JwtAuthGuard, LoggedInGuard } from 'src/auth/guard';
 import { CommentsService } from './comments.service';
 
 @ApiTags('댓글 API')
@@ -71,5 +74,41 @@ export class CommentsController {
   async getUserComments(@Req() req: any) {
     const userId = req.user.userId;
     return this.commentsService.getCommentsByUserId(userId);
+  }
+
+  @UseGuards(JwtAuthGuard, LoggedInGuard)
+  @Delete(':commentId')
+  @ApiOperation({
+    summary: '댓글 삭제 API',
+    description: '사용자가 작성한 댓글을 삭제합니다.',
+  })
+  async deleteComment(
+    @Param('commentId', ParseIntPipe) commentId: number,
+    @Req() req: any,
+  ) {
+    const userId = req.user.userId;
+    await this.commentsService.deleteComment(commentId, userId);
+    return {
+      statusCode: HttpStatus.OK,
+      message: '댓글이 성공적으로 삭제되었습니다.',
+    };
+  }
+
+  @UseGuards(JwtAuthGuard, AdminGuard)
+  @Delete(':commentId')
+  @ApiOperation({
+    summary: '관리자 댓글 삭제 API',
+    description: '관리자가 선택한 댓글을 삭제합니다.',
+  })
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async deleteCommentByAdmin(
+    @Param('commentId') commentId: number,
+    @Req() req: any,
+  ) {
+    await this.commentsService.deleteComment(commentId, req.user.userId);
+    return {
+      statusCode: HttpStatus.NO_CONTENT,
+      message: '댓글이 성공적으로 삭제되었습니다.',
+    };
   }
 }
