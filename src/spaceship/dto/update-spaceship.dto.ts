e6f30d60096e0d2ef678ef1c9019456458db33c5
Hyeongyu-619 <110807563+Hyeongyu-619@users.noreply.github.com>
2023-11-05 18:47:49 +0900

    Fix: 우주선 수정 DTO 수정

--- src/spaceship/dto/update-spaceship.dto.ts
+++ src/spaceship/dto/update-spaceship.dto.ts
@@ -1,32 +1,40 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsDate, IsInt, IsString } from 'class-validator';
+import {
+  IsDate,
+  IsDateString,
+  IsInt,
+  IsOptional,
+  IsString,
+} from 'class-validator';
 
 export class UpdateSpaceshipDto {
-  @ApiProperty({ description: '우주선 이름' })
+  @ApiProperty({ description: '우주선 이름', required: false })
   @IsString()
+  @IsOptional()
   name?: string;
 
-  @ApiProperty({ description: '우주선 설명' })
+  @ApiProperty({ description: '우주선 설명', required: false })
   @IsString()
+  @IsOptional()
   description?: string;
 
-  @ApiProperty({ description: '우주선 최대 인원수' })
+  @ApiProperty({ description: '우주선 최대 인원수', required: false })
   @IsInt()
+  @IsOptional()
   maxMembers?: number;
 
-  @ApiProperty({ description: '우주선 이륙일' })
-  @IsDate()
-  startDate?: Date;
+  @ApiProperty({ description: '우주선 이륙일', required: false })
+  @IsDateString()
+  @IsOptional()
+  startDate?: string;
 
-  @ApiProperty({ description: '우주선 복귀일' })
-  @IsDate()
-  endDate?: Date;
+  @ApiProperty({ description: '우주선 복귀일', required: false })
+  @IsDateString()
+  @IsOptional()
+  endDate?: string;
 
-  @ApiProperty({ description: '우주선이 속한 행성 ID' })
+  @ApiProperty({ description: '우주선이 속한 행성 ID', required: false })
   @IsInt()
+  @IsOptional()
   planetId?: number;
-
-  @ApiProperty({ description: '우주선 대표 이미지' })
-  @IsString()
-  image?: string;
 }
