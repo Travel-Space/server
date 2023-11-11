5a3c0cb442fcb51c94d4bec65ec16b19f249ae51
Hyeongyu-619 <110807563+Hyeongyu-619@users.noreply.github.com>
2023-11-12 02:35:33 +0900

    Fix: 알림 모듈 의존성 주입 해결

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,17 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UserSuspensionService } from './user-suspension.service';
+import { NotificationService } from 'src/notification/notification.service';
+import { NotificationGateway } from 'src/notification/notification.gateway';
 
 @Module({
-  providers: [UserService, PrismaService, UserSuspensionService],
+  providers: [
+    UserService,
+    PrismaService,
+    UserSuspensionService,
+    NotificationService,
+    NotificationGateway,
+  ],
   controllers: [UserController],
   exports: [UserService],
 })
